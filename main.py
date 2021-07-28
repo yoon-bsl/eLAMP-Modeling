cc0f9c57699f4c2f4f87a1457a7bfad9cf7e1d20
Alex Day <alexanderday@email.arizona.edu>
2021-07-28 01:28:44 -0700

    Add argparser and implement oop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import seaborn as sb
 import pandas as pd
 import numpy as np
 import scipy as sp
@@ -18,57 +17,100 @@ class Model(object):
 
     '''
 
-    def __init__(self):
+    def __init__(self, dataFile='dropletDiameters.csv'):
+        self.dataFile = dataFile
+
         self.setSeed()
+
+    def run(self):
+        pass
 
     def setSeed(self):
         random.seed(1234)
 
+    def dropletSizeAnalysis(self):
+         # Import droplet diameters from non-amplified emulsion droplets
+        # containing LAMP reaction mixtures
+        diams = pd.read_csv(self.dataFile, delimiter='\n')
+        diams = pd.DataFrame(diams)
+
+        # Create data column representing the corresponding volume of each
+        # droplet from the diameter
+        microRadius = (diams["Diameter (um)"] / 2)       # get radius in um
+        standardRadius = microRadius / (10 ** 6)    # get radius in meters
+        centiRadius = standardRadius * 100          # radius in centimeters
+        volume = (4/3)*(centiRadius**3)*math.pi     # volume in cm^3 or mL
+        picoVolume = volume * (10**9)               # volume in pL
+
+        diams["Volume (pL)"] = round(picoVolume, 3)
+
+        # Create a data column representing the corresponding surface area
+        # of each droplet from the diameter
+        nanoRadius = (diams["Diameter (um)"] / 2) * 1000    # get radius in nm
+        surfaceArea = 4 * (nanoRadius ** 2) * math.pi       
+        # surface area (nm^3)
+
+        diams["Surface Area (nm^3)"] = round(surfaceArea, 3)
+
+        # Print the various statistics:
+        avgVol = statistics.mean(diams["Volume (pL)"].tolist())
+        avgSA  = statistics.mean(diams["Surface Area (nm^3)"].tolist())
+        medVol = statistics.median(diams["Volume (pL)"].tolist())
+        medSA  = statistics.median(diams["Surface Area (nm^3)"].tolist())
+        print(f'Average volume of measured droplets (in pL):         {avgVol}')
+        print(f'Median volume of measured droplets (in pL):          {medVol}')
+        print(f'Average surface area of measured droplets (in nm^3): {avgSA}')
+        print(f'Median surface area of measured droplets (in nm^3):  {medSA}')
+
+    def calculateGrowthConstant(self, Dt):
+        '''
+        Given a doubling time (in seconds) for LAMP, the function returns the
+        growth constant for the reaciton
+
+        input:
+        Dt = doubling time of the reaction from literature/specifications (s)
+
+        output:
+        k  = growth constant (s^-1)
+        '''
+        return 1/Dt
+
+    def calcaulteDiffusivity(self, bp):
+        '''
+        Given the base pair length of a 
+        '''
+        pass
+
+    def calculate(self, initial, k, D):
+        '''
+        Main amplicon creation/diffusion calculation. Uses same model as in
+        Ulep et al. published in July 2019. Combines Fick's diffusion 
+        equation with exponential growth of LAMP amplicons to model amplicon
+        adsorption to the oil-water interface.
+        '''
+        pass
+
 if __name__ == '__main__':
-    # Set the random seed to make data reproducable
-    random.seed(1234)
+    parser = argparse.ArgumentParser(
+        description='Model Emulsion LAMP amplicon creation and adsorption'
+    )
 
-    # Import droplet diameters from non-amplified emulsion droplets
-    # containing LAMP reaction mixtures
-    diams = pd.read_csv('dropletDiameters.csv', delimiter='\n')
-    diams = pd.DataFrame(diams)
+    parser.add_argument('-i', '--data', required=False, 
+                        help='Droplet data file name')
+    # parser.add_argument('-c', '--column', required=True,
+    #                 help='Image column containing sample colors')
+    # parser.add_argument('-f', '--fristclass', nargs='+', required=True,
+    #                 help='List containing sample #s of the first sample type')
+    # parser.add_argument('-s', '--secondclass', nargs='+', required=True,
+    #                 help='List containing sample #s of the second sample type')
+    # parser.add_argument('-t', '--thirdclass', nargs='+', required=True,
+    #                 help='List containing sample #s of the third sample type')
+    # parser.add_argument('-l', '--lastclass', nargs='+', required=True,
+    #                 help='List containing sample #s of the fourth sample type')
 
-    # Create data column representing the corresponding volume of each
-    # droplet from the diameter
-    microRadius = (diams["Diameter (um)"] / 2)       # get radius in um
-    standardRadius = microRadius / (10 ** 6)    # get radius in meters
-    centiRadius = standardRadius * 100          # radius in centimeters
-    volume = (4/3)*(centiRadius**3)*math.pi     # volume in cm^3 or mL
-    picoVolume = volume * (10**9)               # volume in pL
+    args = parser.parse_args()
 
-    diams["Volume (pL)"] = round(picoVolume, 3)
-
-    # Create a data column representing the corresponding surface area
-    # of each droplet from the diameter
-    nanoRadius = (diams["Diameter (um)"] / 2) * 1000    # get radius in nm
-    surfaceArea = 4 * (nanoRadius ** 2) * math.pi       # surface area (nm^3)
-
-    diams["Surface Area (nm^3)"] = round(surfaceArea, 3)
-
-    # Tell the user the average and median volume/surface area of the droplets
-    avgVol = statistics.mean(diams["Volume (pL)"].tolist())
-    avgSA  = statistics.mean(diams["Surface Area (nm^3)"].tolist())
-    medVol = statistics.median(diams["Volume (pL)"].tolist())
-    medSA  = statistics.median(diams["Surface Area (nm^3)"].tolist())
-    print(f'Average volume of measured droplets (in pL):         {avgVol}')
-    print(f'Median volume of measured droplets (in pL):          {medVol}')
-    print(f'Average surface area of measured droplets (in nm^3): {avgSA}')
-    print(f'Median surface area of measured droplets (in nm^3):  {medSA}')
-
-    # sb.set_theme(style="darkgrid")
-
-    # plt.figure()
-    # ax = sb.boxplot(x=temp)
-    # plt.show()
-
-    parser = argparse.ArgumentParser(description='Input for eLAMP monitoring')
-    parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                        help='an integer for the accumulator')
-    parser.add_argument('--sum', dest='accumulate', action='store_const',
-                        const=sum, default=max,
-                        help='sum the integers (default: find the max)')
+    model = Model(
+        args.data       
+        )
+    model.run()
